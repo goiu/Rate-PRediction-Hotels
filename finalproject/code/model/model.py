4eb5053c55a5c9c54d73643e34ab2aeb56e799e3
justenjoffe <90436945+justenjoffe@users.noreply.github.com>
2023-05-02 18:56:38 -0400

    Add files via upload

---
+++ finalproject/code/model/model.py
@@ -0,0 +1,233 @@
+import numpy as np
+import pandas as pd
+import torch
+# from tensorflow import 
+import tensorflow as tf
+# from tensorflow import LSTM, Linear, Dropout, MaxPool1d, GRU, Conv1d, Embedding, Sequential, ReLU, Softmax, Sigmoid
+from preprocessing import preprocess
+from torch.utils.data import DataLoader
+from matplotlib import pyplot as plt
+
+
+#constants
+GPU = False
+MAX_WORDS = 50
+#Depends on which classification (2, 3 or 5)
+NUMBER_OF_CLASSES = 3
+#Set large vocab size for embedding matrix
+VOCAB_SIZE = 500000
+EPOCHS = 50
+BATCH_SIZE = 100
+
+class Model(tf.keras.Model):
+    def __init__(self, classification):
+        super(Model, self).__init__()
+        #Define batch size
+        self.batch_size = 100
+        self.linear_size_one = 300 
+        self.linear_size_two = 100 
+        
+        #Define embedding matrix
+        self.embedding = tf.keras.layers.Embedding(VOCAB_SIZE, 128)
+        
+        #Define layers
+        #Type error
+        # self.LSTM = tf.keras.layers.LSTM((MAX_WORDS*128, self.linear_size_one))
+        self.LSTM = tf.keras.layers.LSTM(self.linear_size_one)
+        self.l1 = tf.keras.layers.Dense(self.linear_size_one, self.linear_size_two)
+        self.relu = tf.keras.layers.ReLU()
+
+        #Pass in the number of output classes
+        self.l2 = tf.keras.layers.Dense(self.linear_size_two, classification)
+
+        #Sigmoid for binary, softmax for multi-class
+        self.softmax = tf.keras.layers.Softmax()
+        self.sigm = tf.keras.activations.sigmoid()
+
+        self.optimizer = tf.keras.optimizers.Adam()
+        #Loss list for plotting the losses
+        self.loss_list = []
+
+    def call(self, reviews):
+        #The shape of the self.embedding output will be [sentence_length, batch_size, embedding_dim]
+        l1_out = self.embedding(reviews)
+        l1_out = tf.reshape(l1_out, (self.batch_size, MAX_WORDS*128))
+
+        #Pass inputs through LSTM
+        l2_out, hidden_state = self.LSTM(l1_out)
+        
+        #Pass through dense layers
+        l3_out = self.l1(l2_out)
+       
+        l4_out = self.relu(l3_out)
+       
+        l5_out = self.l2(l4_out) 
+
+        #Use softmax to get probability distribution 
+        final_out = self.softmax(l5_out)
+
+        
+        return final_out
+
+    def loss(self, labels, predictions):
+
+        #Convert labels to tensor
+        labels = tf.Variable(labels)
+        loss = tf.keras.losses.CategoricalCrossentropy()
+        output = loss(predictions, labels)
+        return output
+
+
+    def accuracy(self, labels, predictions):
+        
+        correct_count = 0
+        count = 0
+        #Run through each input in batch
+        for i in range(len(predictions)):
+            #Helps to indicate which predictions are correct/shows what the model learned for that input
+            print(predictions[i])
+            print(labels[i])
+            print(tf.argmax(predictions[i]).item())
+            #Returns the indices of the maximum value thus if correctly predicted increments counter
+            if tf.argmax(predictions[i]).item() == labels[i]:
+                correct_count += 1
+            count += 1
+        
+        #Return the correct predictions over total to give accuracy for that batch
+        return correct_count/count
+
+
+    def train_torch(self, inputs, labels):
+        #Define optimizer to use in backpropogation
+        optimizer = tf.optimizers.Adam(learning_rate=0.005)
+        for i in range(int(len(inputs)/self.batch_size)):
+            print("Training batch: ", i)
+
+            #Get the next batch of inputs and labels
+            input_batch = inputs[i*self.batch_size: i*self.batch_size + self.batch_size]
+            labels_batch = labels[i*self.batch_size: i*self.batch_size + self.batch_size]
+   
+            #Convert inputs to tensor
+            input_batch = torch.tensor(input_batch)
+           
+            #Run forward pass
+            probabilites = self.call(input_batch)
+
+            #Calculate the loss
+            loss = self.loss(labels_batch, probabilites)
+            print("Loss from batch: ", i, "i", loss)
+            self.loss_list.append(loss.item())
+            
+            #Update model parameters
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        return 
+    
+
+
+    
+    # def test_torch(self, inputs, labels):
+    #     accuracy_list = []
+
+    #     for i in range(int(len(inputs)/self.batch_size)):
+    #         print("Testing batch: ", i)
+
+    #         #Get next batch of inputs and labels
+    #         input_batch = inputs[i*self.batch_size: i*self.batch_size + self.batch_size]
+    #         labels_batch = labels[i*self.batch_size: i*self.batch_size + self.batch_size]
+
+    #         #Convert inputs to tensor to run through model
+    #         input_batch = torch.tensor(input_batch)
+
+    #         #Get the probability distribution for the batch
+    #         probabilites = self.call(input_batch)
+
+    #         #Calculate the accuracy
+    #         accuracy = self.accuracy(labels_batch, probabilites)
+    #         print("batch accuracy: ", accuracy)
+            
+    #         #Append accuracy to list
+    #         accuracy_list.append(accuracy)
+        
+    #     #Return the average accuracy across all batches
+    #     return np.average(accuracy_list)
+
+def test(model, test_inputs, test_labels):
+    """
+    Tests the model on the test inputs and labels. You should NOT randomly 
+    flip images or do any extra preprocessing.
+    
+    :param test_inputs: test data (all images to be tested), 
+    shape (num_inputs, width, height, num_channels)
+    :param test_labels: test labels (all corresponding labels),
+    shape (num_labels, num_classes)
+    :return: test accuracy - this should be the average accuracy across
+    all batches
+    """
+    test_accuracy = []
+    num_batches = int(len(test_inputs)/model.batch_size)
+    for batch in range(num_batches):
+        low = model.batch_size * batch
+        high =  np.multiply(model.batch_size, batch) + model.batch_size
+        logit = model.call(test_inputs[low:high], True)
+        accuracy = model.accuracy(logit, test_labels[low:high])
+        test_accuracy.append(accuracy)
+    return_me = np.average(test_accuracy)
+    return return_me
+def train(model, inputs, labels):
+    tl = 0 
+    for i in range(int(len(inputs)/model.batch_size)):
+        print("Training batch: ", i)
+        input_batch = inputs[i*model.batch_size: i*model.batch_size + model.batch_size]
+        labels_batch = labels[i*model.batch_size: i*model.batch_size + model.batch_size]
+        input_batch = tf.Variable(input_batch)
+        with tf.GradientTape() as tape:
+            probs = model.call(input_batch)
+            loss = model.loss(labels_batch, probs)
+            model.loss_list.append(loss)
+            gradients = tape.gradient(loss, model.trainable_variables)
+            model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    return model.loss_list
+
+
+
+
+def visualize_loss(losses): 
+    """
+    Uses Matplotlib to visualize the losses of our model.
+    
+    """
+    #losses = losses.numpy()
+    x = [i for i in range(len(losses))]
+    plt.plot(x, losses)
+    plt.title('Loss per batch')
+    plt.xlabel('Batch')
+    plt.ylabel('Loss')
+    plt.show()  
+
+def main():
+    
+    print("called main")
+
+    #Instantiate the model
+    #Change classification to be number of classes you want model to differentiate between
+    model = Model(classification=2)
+
+    #Get the train and test inputs and labels from preprocess
+    #Preprocesses labels depending on what type of classification: binary/multi-class
+    train_inputs, test_inputs, train_labels, test_labels = preprocess(classification=2)
+
+    #Train the model
+
+    
+    #Get the accuracy from testing
+    for x in range(3):
+        train(model, train_inputs, train_labels)
+    accuracy = test(test_inputs, test_labels)
+    visualize_loss(model.loss_list)
+    print("accuracy", accuracy)
+     
+if __name__ == "__main__":
+    main()
---
+++ finalproject/code/model/preprocessing.py
@@ -0,0 +1,148 @@
+from concurrent.futures import process
+from sre_parse import Tokenizer
+import pandas as pd
+import numpy as np
+import pickle
+import nltk
+import re
+import string
+import torch
+import tensorflow
+from keras.preprocessing.text import Tokenizer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+from torch.nn.utils.rnn import pad_sequence
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
+
+#In order to preprocess data we have a number of things we want to do:
+# 1. remove punctuation
+# 2. remove stop_words (common words)
+# 3. stem - get the stem of words
+# 4. lemmatize  
+
+def get_labels_data():
+    #Read from the csv file to create a pandas dataframe
+    data = pd.read_csv('/Users/justenjoffe/Downloads/yip-yap-yelp-main/code/data/yelp_reviews_Hotels_categories.csv')
+
+    #get the labels - corresponds to number of stars 
+    labels = data['review_stars'].values
+    reviews = data['text']
+    
+    #get rid of all punctuation
+    reviews = data['text'].apply(lambda x: re.sub('[%s]' % re.escape(string.punctuation), '' , x))
+    
+    #Determines how many reviews we want running through the model
+    return labels[0:80000], reviews[0:80000]
+    
+#Function only for use if classifying as positive or negative
+def binary_label(labels):
+
+    labels_list= []
+    for label in labels:
+        if label >= 3:
+            labels_list.append(0)
+        else:
+            labels_list.append(1)
+    
+    return np.array(labels_list)
+
+#Function for classifying 3 classes: positive, neutral or negative
+def ternary_label(labels):
+    labels_list= []
+    for label in labels:
+        if label > 3:
+            labels_list.append(0)
+        if label == 3:
+            labels_list.append(1)
+        if label < 3:
+            labels_list.append(2)
+    return np.array(labels_list)
+
+#For classifying all 5 star rating classes - sets them from 0-4 as this is what is used for loss
+def five_classes(labels):
+    labels_list= []
+    for label in labels:
+        if label == 5:
+            labels_list.append(0)
+        if label == 4:
+            labels_list.append(1)
+        if label == 3:
+            labels_list.append(2)
+        if label == 2:
+            labels_list.append(3)
+        if label == 1:
+            labels_list.append(4)
+    
+    return np.array(labels_list)
+
+def process_text(reviews):
+    #Get the stop words 
+    stop_words = set(stopwords.words('english'))
+    filtered_reviews = []
+
+    for review in reviews:
+        
+        #Tokenize the input
+        tokenized_review = word_tokenize(review)
+
+        #Remove the stop words
+        filtered_sentence = [w for w in tokenized_review if not w.lower() in stop_words]
+
+        #Stem the sentence or lemmatize, this helps to stop 'trailing e' issue:
+        ps = PorterStemmer()
+        lemmatizer = WordNetLemmatizer()
+        filtered = [lemmatizer.lemmatize(w) if lemmatizer.lemmatize(w).endswith('e') else ps.stem(w) for w in filtered_sentence]
+        filtered_reviews.append(filtered)
+
+    #Now we have removed all words we do not want we need to convert our reviews into sequence
+    t = Tokenizer()
+    #Updates internal vocabulary based on a list of texts. 
+    t.fit_on_texts(filtered_reviews)
+    #Converts reviews 
+    sequenced_reviews = t.texts_to_sequences(filtered_reviews)
+    
+    #Finally we need to add padding to ensure all our reviews are the same length
+    #Also set the max length of the review here, parameter that can be altered
+    padded_reviews = pad_sequences(sequenced_reviews, maxlen=50) 
+    
+    return padded_reviews
+
+def preprocess(classification=2):
+
+    labels, reviews = get_labels_data()
+
+    #Call if binary classification to convert labels
+    if classification == 2:
+        labels = binary_label(labels)
+
+    #Call if ternary classification
+    if classification == 3:
+        labels = ternary_label(labels)
+
+    #Call if full multi-class classification
+    if classification == 5:
+        labels = five_classes(labels)
+
+    #process the reviews so they can ba parsed through the model
+    reviews = process_text(reviews)
+    
+    #Split the reviews into train and test  
+    train_inputs = reviews[:64000]
+    test_inputs = reviews[64000:]
+
+    #Split corresponding labels into train and test
+    train_labels = labels[:64000]
+    test_labels = labels[64000:]
+
+    return train_inputs, test_inputs, train_labels, test_labels
+
+
+
+
